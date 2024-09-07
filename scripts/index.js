14086e3f5931dfe53ca0d5ffb94c1920bef7173c
aumer-amr <aumer@aumer.dev>
2024-09-07 11:37:14 +0200

    feat: alter temp hack script to include pass reset

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const ping = require('ping');
 const proxmox = require('proxmox-for-node').default;
+const k8s = require('@kubernetes/client-node');
+const { spawnSync } = require('child_process');
+
+const kc = new k8s.KubeConfig();
+kc.loadFromFile("../kubeconfig");
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+const k8sCustomApi = kc.makeApiClient(k8s.CustomObjectsApi);
 
 (async () => {
   const selectedHost = await selectHost(process.env.PROXMOX_HOSTS.split(','));
@@ -14,10 +22,110 @@ const proxmox = require('proxmox-for-node').default;
     api: process.env.PROXMOX_API
   });
 
-  rebootNotReadyNodes();
+  await rebootNotReadyNodes();
+
+  await waitForNodesToBeReady();
+
+  await resetPostgresPassword();
+
+  await waitPodsToBeReady();
+
+  async function waitPodsToBeReady() {
+    return new Promise(async (resolve, reject) => {
+      let tries = 0;
+      let notReadyPods = await getNotReadyPods();
+      while (notReadyPods.length > 0 || tries > 48) {
+        tries++;
+        console.log(`Waiting for pods ${notReadyPods} to be ready...`);
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+        notReadyPods = await getNotReadyPods();
+      }
+
+      if (tries > 48) {
+        return reject(new Error(`Pods ${notReadyPods} did not become ready in time.`));
+      }
+
+      console.log('All pods ready');
+      return resolve();
+    });
+  }
+
+  async function getNotReadyPods() {
+    const notReadyPods = [];
+    const pods = await k8sApi.listPodForAllNamespaces();
+    pods.body.items.forEach((pod) => {
+      if (pod.status.conditions.filter((condition) => condition.type === 'Ready' && condition.status === 'False' && condition.reason !== "PodCompleted").length > 0) {
+        console.log(`Pod ${pod.metadata.name} is not ready.`);
+        notReadyPods.push(pod.metadata.name);
+      }
+    });
+    return notReadyPods;
+  }
+
+  async function resetPostgresPassword() {
+    return new Promise(async (resolve, reject) => {
+      const cluster = await k8sCustomApi.getNamespacedCustomObject('postgresql.cnpg.io', 'v1', 'database', 'clusters', 'postgres');
+      const primary = cluster.body.status.targetPrimary;
+
+      console.log(`Found primary database node: ${primary}`);
+
+      const psqlCommand = `psql -c "ALTER USER postgres WITH PASSWORD '${process.env.POSTGRES_PASS}';"`;
+      const commandArgs = ['exec', primary, '-n', 'database', '--', 'bash', '-c', psqlCommand];
+
+      const result = spawnSync('kubectl', commandArgs, { encoding: 'utf-8' });
+
+      // Handle the result
+      if (result.error) {
+        console.error(`Execution error: ${result.error.message}`);
+        return reject();
+      }
+
+      if (result.stdout.includes('ALTER ROLE')) {
+        console.log('Password reset successfully.');
+        return resolve();
+      }
+
+      if (result.status !== 0) {
+        console.error('Command failed.');
+        return reject();
+      }
+    });
+  }
+
+  async function waitForNodesToBeReady() {
+    return new Promise(async (resolve, reject) => {
+      let tries = 0;
+      let notReadyNodes = await getK8sNotReadyNodes();
+      while (notReadyNodes.length > 0 || tries > 12) {
+        tries++;
+        console.log(`Waiting for nodes ${notReadyNodes} to be ready...`);
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+        notReadyNodes = await getK8sNotReadyNodes();
+      }
+
+      if (tries > 12) {
+        return reject(new Error(`Nodes ${notReadyNodes} did not become ready in time.`));
+      }
+
+      console.log('All nodes ready');
+      return resolve();
+    });
+  }
+
+  async function getK8sNotReadyNodes() {
+    const notReadyNodes = [];
+    const nodes = await k8sApi.listNode();
+    nodes.body.items.forEach((node) => {
+      if (node.status.conditions.filter((condition) => condition.type === 'Ready' && condition.status === 'False').length > 0) {
+        console.log(`Node ${node.metadata.name} is not ready.`);
+        notReadyNodes.push(node.metadata.name);
+      }
+    });
+    return notReadyNodes;
+  }
 
   async function rebootNotReadyNodes() {
-    const notReadNodes = await kubectlGetNotReadyNodes();
+    const notReadNodes = await getK8sNotReadyNodes();
     const vmIds = await collectVmIds();
     for await (const node of notReadNodes) {
       const vm = vmIds[`${node}-k8s`];
@@ -95,19 +203,6 @@ const proxmox = require('proxmox-for-node').default;
           return reject(new Error('Not all nodes are online.'));
         }
         return resolve(data.filter((node) => node.type === 'node' && node.online === 1).map((node) => node.name));
-      });
-    });
-  }
-
-  function kubectlGetNotReadyNodes() {
-    process.chdir('../');
-    return new Promise((resolve, reject) => {
-      const { exec } = require('child_process');
-      exec('kubectl get nodes | grep NotReady | awk \'{ print $1 }\'', (error, stdout, stderr) => {
-        if (error) {
-          return reject(error);
-        }
-        return resolve(stdout.split('\n').filter(Boolean));
       });
     });
   }
